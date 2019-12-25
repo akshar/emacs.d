43e088a201e0724b37349fee300ed8624d3aa392
Akshar Dave <akshar314@gmail.com>
2019-12-25 22:03:34 +0530

    Add ido

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (setq inhibit-startup-message t)
+(tool-bar-mode -1)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -10,3 +11,29 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+
+(use-package try
+  :ensure t)
+
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+
+;; Org-mode
+
+(use-package org-bullets
+  :ensure t
+  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
+;; IDO
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(defalias 'list-buffers 'ibuffer)
+
+
