d6c99ee0dcad03c032c9f67131ae20e72e0fd2a8
Akshar Dave <akshar314@gmail.com>
2020-01-02 21:12:23 +0530

    Move base config to org file

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
 
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-;;M-x  package-refresh-contents -> to refresh packages
+
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -13,86 +11,6 @@
   (package-install 'use-package))
 
 
-(use-package try
-  :ensure t)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-
-;; Org-mode
-
-(use-package org-bullets
-  :ensure t
-  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-
-;; IDO
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-(defalias 'list-buffers 'ibuffer)
-(windmove-default-keybindings) ;; shift+ arrow keys to movearound buffers
-
-
-(use-package counsel
-  :ensure t)
-
-
-;; swiper
-
-(use-package swiper
-  :ensure t
-  :config (ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
-
-;; autocomplete
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)))
-
-(use-package zenburn-theme
-  :ensure t
-  :config (load-theme 'zenburn t))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zenburn-theme auto-complete counsel swiper org-bullets which-key use-package try))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
