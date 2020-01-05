3e94f2ab68a3ff2b72bf764792c2361cd6493209
Akshar Dave <akshar314@gmail.com>
2020-01-05 10:13:53 +0530

    Add elpa

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 
 (package-initialize)
 
